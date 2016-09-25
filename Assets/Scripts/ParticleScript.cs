df3a5c144e0c11a09b3258c26e37ae2a963046cf
4000T-69302\Comp30019 <hugz98@hotmail.com>
2016-09-25 13:58:18 +1000

    Added spark particles. Presently they just spawn when you press space - when we re-implement the walls we can make them burst on collision

---
+++ Assets/Scripts/ParticleScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleScript : MonoBehaviour
+{
+
+    //Simple script - camera follows ball
+
+    public GameObject following;
+
+    public int BurstNumber;
+
+    private Vector3 offset;
+
+    private ParticleSystem p;
+
+    void Start()
+    {
+        offset = transform.position - following.transform.position;
+        p = GetComponent<ParticleSystem>();
+        p.Play();
+        p.emissionRate = 0;
+
+    }
+
+    void Update()
+    {
+        transform.position = following.transform.position + offset;
+
+        if (Input.GetKeyDown("space")) {
+            p.Emit(BurstNumber);
+            
+        }
+
+    }
+
+   
+
+}
