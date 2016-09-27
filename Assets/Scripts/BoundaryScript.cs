41323cd10aaba3a77b831696b6b527f9df390103
Daniel <danielporteous1@gmail.com>
2016-09-27 17:03:35 +1000

    Made the kill wall below move with the player. Also made obstacles spawn further away.

--- Assets/Scripts/BoundaryScript.cs
+++ Assets/Scripts/BoundaryScript.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class BoundaryScript : MonoBehaviour {
 
-	void OnTriggerExit(Collider other)
-    {
+    public PlayerController player;
+    private float offset;
+
+	void OnTriggerExit(Collider other) {
         if (other.gameObject.tag == "obstacle") {
             Destroy(other.gameObject);
         }
+    }
 
+    void Start() {
+        offset = transform.position.z - player.transform.position.z;
+    }
+
+    void Update() {
+        transform.position = new Vector3(0.0f, 0.0f, (player.transform.position.z + offset));
     }
 }
