fa9da214130dbb7d9da9835fcd5f1f8fa1da7579
Daniel <danielporteous1@gmail.com>
2016-10-01 10:42:16 +1000

    Added trees to improve the downhill illusion. Haven't figured out how to colour them properly yet.

---
+++ Assets/Scripts/TreeScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeScript : MonoBehaviour {
+
+    //slope script - passes stuff to shader
+
+    public Shader shader;
+    public PointLight light; //TODO im not sure this does anything?
+
+    MeshRenderer rend;
+
+    void Start() {
+
+        rend = GetComponent<MeshRenderer>();
+        rend.material.shader = shader;
+
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        Color[] colors = new Color[vertices.Length];
+
+        // This doesn't even do anything TODO, it is overwritten in update.
+        for (int i = 0; i < vertices.Length; i++)
+            colors[i] = new Color(0.5F, 0.5F, 0.5F, 1.0F);
+
+        mesh.colors = colors;
+
+    }
+
+
+    void Update() {
+        rend.material.SetColor("_PointLightColor", this.light.color);
+        rend.material.SetVector("_PointLightPosition", this.light.GetWorldPosition());
+    }
+
+}
