9c04378909e53e6c38231d5b71c57ffad68b2a0e
4000T-69302\Comp30019 <hugz98@hotmail.com>
2016-10-05 22:48:30 +1100

    Added fog. Yew.

---
+++ Assets/Scripts/FogController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogController : MonoBehaviour {
+
+    public Color fogColor = Color.red;
+    public float fogDensity;
+
+    void Start()
+    {
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = fogColor;
+        RenderSettings.fogDensity = fogDensity;
+    }
+
+}
