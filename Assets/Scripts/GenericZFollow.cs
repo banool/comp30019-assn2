96b99cd8ff8b37f39095b47c9e41180e9defe025
Daniel <danielporteous1@gmail.com>
2016-10-08 14:34:20 +1100

    Reorganised file structure.

---
+++ Assets/Scripts/GenericZFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericZFollow : MonoBehaviour
+{
+
+    public PlayerController following;
+    private float offset;
+    private float startX;
+    private float startY;
+
+    // Use this for initialization
+    void Start() {
+        offset = transform.position.z - following.transform.position.z;
+        startX = transform.position.x;
+        startY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = new Vector3(startX, startY, following.transform.position.z + offset);
+    }
+}
